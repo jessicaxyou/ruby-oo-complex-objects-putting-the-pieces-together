b430df7314bb6719fbcbedba3808b7b36ed0fab9
jessica you <jessicaxytan@gmail.com>
2021-03-03 19:28:30 -0500

    commit tree fbfc290685f7327649f3f58cc2643f2a63e5ba51

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+require 'pry'
 
+class Book
+
+    def initialize(title)
+        @title=title #@title is the instance variable of title that sets initialize default
+    end
+
+    attr_reader :title
+    # def title #getting as it gets the value of title for us
+    #     @title
+    # end
+
+    attr_accessor :author, :page_count, :genre
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+
+require 'pry'
+
+class Shoe
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    attr_reader :brand
+
+    attr_accessor :color, :size, :material, :condition
+
+    def cobble
+        self.condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+
+end
